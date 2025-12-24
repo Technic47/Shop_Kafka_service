bf19476a3d6cd3751dc7b7a48050e29827fd2534
Pavel Kuznetsov <Technic47@gmail.com>
2025-12-24 15:42:59 +0300

    Init commit.

---
+++ src/test/java/ru/kuznetsov/shop/kafka/KafkaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kuznetsov.shop.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
